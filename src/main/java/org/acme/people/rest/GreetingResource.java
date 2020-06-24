f437b69a13d75439a6e9e0724feb6f7e777da7e1
darbyredhat <cdarby@redhat.com>
2020-06-24 15:30:18 -0400

    updates

--- src/main/java/org/acme/people/rest/GreetingResource.java
+++ src/main/java/org/acme/people/rest/GreetingResource.java
@@ -5,17 +5,29 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Optional;
 
 @Path("/hello")
 public class GreetingResource {
 
     public static final Logger log = LoggerFactory.getLogger(GreetingResource.class);
 
+    @ConfigProperty(name = "greeting.message")
+    String message;
+
+    @ConfigProperty(name = "greeting.suffix", defaultValue="!")
+    String suffix;
+
+    @ConfigProperty(name = "greeting.name")
+    Optional<String> name;
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "hello";
+        return message + " " + name.orElse("world") + suffix;
     }
 }
--- src/test/java/org/acme/people/GreetingResourceTest.java
+++ src/test/java/org/acme/people/GreetingResourceTest.java
@@ -17,7 +17,7 @@ public class GreetingResourceTest {
           .when().get("/hello")
           .then()
              .statusCode(200)
-             .body(is("hello"));
+             .body(is("hello quarkus!")); // Modified line
     }
 
     // add more tests
