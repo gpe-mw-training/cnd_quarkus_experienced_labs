ff48655d72934fd5e13b4a498c092fc8cb6ad551
darbyredhat <cdarby@redhat.com>
2020-06-24 19:12:32 -0400

    updates

--- deploy-lab-03-02.sh
+++ deploy-lab-03-02.sh
@@ -13,8 +13,6 @@ oc new-app \
 
 mvn clean package -DskipTests
 
-oc new-build registry.access.redhat.com/openjdk/openjdk-11-rhel7:1.1 --binary --name=people
-
 oc start-build people --from-file target/*-runner.jar --follow
 
 oc rollout status -w dc/people
