91af337798705df1acc81672762f28f356434b2b
darbyredhat <cdarby@redhat.com>
2020-06-24 19:02:26 -0400

    updates

--- src/main/java/org/acme/people/rest/PersonResource.java
+++ src/main/java/org/acme/people/rest/PersonResource.java
@@ -49,7 +49,56 @@ public class PersonResource {
     }
 
     // TODO: add datatable query
+    @GET
+    @Path("/datatable")
+    @Produces(MediaType.APPLICATION_JSON)
+    public DataTable datatable(
+            @QueryParam(value = "draw") int draw,
+            @QueryParam(value = "start") int start,
+            @QueryParam(value = "length") int length,
+            @QueryParam(value = "search[value]") String searchVal
+
+    ) {
+        // TODO: Begin result
+        DataTable result = new DataTable();
+        result.setDraw(draw);
+
+        // TODO: Filter based on search
+        PanacheQuery<Person> filteredPeople;
+
+        if (searchVal != null && !searchVal.isEmpty()) {
+            filteredPeople = Person.<Person>find("name like :search",
+                    Parameters.with("search", "%" + searchVal + "%"));
+        } else {
+            filteredPeople = Person.findAll();
+        }
+
+        // TODO: Page and return
+        int page_number = start / length;
+        filteredPeople.page(page_number, length);
+
+        result.setRecordsFiltered(filteredPeople.count());
+        result.setData(filteredPeople.list());
+        result.setRecordsTotal(Person.count());
+
+        return result;
+
+    }
+
 
     // TODO: Add lifecycle hook
+    @Transactional
+    void onStart(@Observes StartupEvent ev) {
+        for (int i = 0; i < 1000; i++) {
+            String name = CuteNameGenerator.generate();
+            LocalDate birth = LocalDate.now().plusWeeks(Math.round(Math.floor(Math.random() * 20 * 52 * -1)));
+            EyeColor color = EyeColor.values()[(int)(Math.floor(Math.random() * EyeColor.values().length))];
+            Person p = new Person();
+            p.birth = birth;
+            p.eyes = color;
+            p.name = name;
+            Person.persist(p);
+        }
+    }
 
 }
